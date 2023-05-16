9762b2c8effb9fc32f9e34da802f1c45f02e67f1
mauriciogoulart <mauryciorgoulart@yahoo.com.br>
2023-05-16 15:24:16 -0300

    initial commit

---
+++ iacl2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Atualizando Servidor"
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando Apache"
+apt-get install apache2 -y
+
+echo "Instalando unzip"
+apt-get install unzip -y
+
+echo "Baixando aplicacao para pasta tmp"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Descompactando aplicacao"
+unzip main.zip
+
+echo "Copiando arquivos para pasta padrao do Apache"
+cd linux-site-dio
+cp -R * /var/www/html/
